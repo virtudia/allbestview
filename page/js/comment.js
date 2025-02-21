4ac9e0f8cab857376db5fa9c5fc5c386ec43dae3
Virtudia <68125721+virtudia@users.noreply.github.com>
2025-02-22 02:53:03 +0800

    Update comment.js

--- page/js/comment.js
+++ page/js/comment.js
@@ -1,4 +1,4 @@
-(function() { // DON'T EDIT BELOW THIS LINE
+(function() { 
     var d = document, s = d.createElement('script');
     s.src = 'https://allbestview.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
